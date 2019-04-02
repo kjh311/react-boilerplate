f61d8234c227d73b39a579533adb139aedd1c3ae
Kevin <kjh311@gmail.com>
2019-04-02 13:12:42 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.scss';
-import Nav from './nav';
-import Heading from './heading';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav />
-        <Heading />
+        <h1>Welcome to my React BoilerPlate with Bootstrap 4 and Sass</h1>
       </div>
     );
   }
